1cb5e145b58c173f854f788605c9c6f0422daafe
John MacGregor <jonathan.macgregor@accenture.com>
2019-04-17 13:28:05 -0400

    Refactored tests

--- BowlingGameTest/BowlingTests.cs
+++ BowlingGameTest/BowlingTests.cs
@@ -9,17 +9,22 @@ namespace BowlingGameTest
 {
     public class BowlingTests
     {
+        private Game _game = new Game();
+        
+        private void RollLots(int rolls, int pins)
+        {
+            for (var i = 0; i < rolls; i++)
+            {
+                _game.Roll(pins);
+            }
+        }
+        
         [Fact]
         public void WhenRollingAllGutterBallsScoreIsZero()
         {
-            var game = new Game();
+            RollLots(20, 0);
 
-            for (var i = 0; i < 20; i++)
-            {
-                game.Roll(0);
-            }
-
-            var result = game.Score();
+            var result = _game.Score();
             
             result.ShouldBe(0);
         }
@@ -27,15 +32,11 @@ namespace BowlingGameTest
         [Fact]
         public void WhenRollingOnePinScoreIsOne()
         {
-            var game = new Game();
             
-            game.Roll(1);
-            for (var i = 0; i < 19; i++)
-            {
-                game.Roll(0);
-            }
-
-            var result = game.Score();
+            _game.Roll(1);
+            RollLots(19,0);
+            
+            var result = _game.Score();
             
             result.ShouldBe(1);
         }
@@ -43,14 +44,9 @@ namespace BowlingGameTest
         [Fact]
         public void WhenRollingOnePinTwentyTimesScoreIsTwenty()
         {
-            var game = new Game();
-            
-            for (var i = 0; i < 20; i++)
-            {
-                game.Roll(1);
-            }
+            RollLots(20, 1);
 
-            var result = game.Score();
+            var result = _game.Score();
             
             result.ShouldBe(20);
         }
