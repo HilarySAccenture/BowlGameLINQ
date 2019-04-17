0d6fa96e707f73e61685affa256d9900ffd5ff35
John MacGregor <jonathan.macgregor@accenture.com>
2019-04-17 13:18:00 -0400

    Gutterball game scores zero

--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -7,35 +7,16 @@ namespace BowlingGame
 {
     public class Game
     {
-        List<int> rolls = new List<int>();
-
+        private int _currentScore;
+        
         public void Roll(int pins)
         {
-            rolls.Add(pins);
+            _currentScore += pins;
         }
 
         public int Score()
         {
-            var currentScore = 0;
-            var currentId = 0;
-
-            var firstRolls= rolls.Where((roll, i) => i % 2 == 0).ToList();
-            var secondRolls = rolls.Where((roll, i) => (i + 1) % 2 == 0).ToList();
-
-            var frames = firstRolls.Select(roll => new
-                {rollOne = roll, rollTwo = secondRolls[firstRolls.IndexOf(roll)], frameIndex = currentId++}).ToList();
-
-            var openFrames = frames.Where(frame => frame.rollOne + frame.rollTwo != 10);
-            currentScore += openFrames.Sum(frame => frame.rollOne + frame.rollTwo);
-
-            //replace openframes with currentscore, tack on sum to line 28
-            var spares = frames.Where(frame => frame.rollOne + frame.rollTwo == 10);
-            currentScore += spares.Sum(frame => 10 + frames[frame.frameIndex + 1].rollOne);
-            
-            //where can I combine these into a single step? 
-            
-            return currentScore; 
-            
+            return _currentScore;
         }
 
     }
--- BowlingGameTest/BowlingTests.cs
+++ BowlingGameTest/BowlingTests.cs
@@ -9,57 +9,20 @@ namespace BowlingGameTest
 {
     public class BowlingTests
     {
-        private readonly Game game = new Game();
-        private int rollCount = 0;
-
         [Fact]
-        public void RollOnePinOnlyScoreIsOne()
+        public void WhenRollingAllGutterBallsScoreIsZero()
         {
-        Game game = new Game();
-            game.Roll(1);
-            rollMany(game, 19, 0);
-            
-            var actualScore = game.Score();
-            
-            actualScore.ShouldBe(1);
-        }
+            var game = new Game();
 
-        [Fact]
-        public void RollOnePinTwentyTimesScoreIsTwenty()
-        {
-            Game game = new Game();
-            rollMany(game, 20, 1);
-
-            var actualScore = game.Score();
-            
-            actualScore.ShouldBe(20);
-        }
-
-        [Fact]
-        public void RollASpareThenRollOnePinScoreIsTwelve()
-        {
-            Game game = new Game();
-            game.Roll(5);
-            game.Roll(5);
-            game.Roll(1);
-            rollMany(game, 17, 0);
-
-            var actualScore = game.Score();
-            
-            actualScore.ShouldBe(12);
-
-        }
-
-        private static void rollMany(Game game, int rollCount, int pins)
-        {
-            for (int i = 0; i < rollCount; i++)
+            for (var i = 0; i < 20; i++)
             {
-                game.Roll(pins);
+                game.Roll(0);
             }
-        } 
-        
-   
-        
+
+            var result = game.Score();
+            
+            result.ShouldBe(0);
+        }
     }
 
     
